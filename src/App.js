a6df451f5b6e7fb14b23095dec6d435b17c54236
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-06-15 04:44:56 -0500

    added-togle-function

--- src/App.js
+++ src/App.js
@@ -5,15 +5,30 @@ import {AmplifySignOut,withAuthenticator} from '@aws-amplify/ui-react'
 import {listMedias} from './graphql/queries';
 import {updateMedia} from './graphql/mutations';
 import {Paper,IconButton,} from '@material-ui/core';
-import PlayArrow from '@material-ui/icons/PlayArrow'
+import PlayArrowIcon from '@material-ui/icons/PlayArrow'
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import  PauseIcon from '@material-ui/icons/Pause';
 import './App.css';
 Amplyfy.configure(awsconfig)
+
 function App() {
   const [medias,setMedia] =useState([])
+  const [mediaPlaying,setMediaPlaying] = useState('')
+  
   useEffect(()=>{
     getAll()
   },[])
+  const toggleMedia = async (id) => {
+    if(mediaPlaying === id){
+      setMediaPlaying('')
+      return 
+    }
+    setMediaPlaying(id)
+    return
+  }
+
+
+  
 const getAll = async() =>{
 try {
   const mediaData = await API.graphql(graphqlOperation(listMedias))
@@ -55,8 +70,8 @@ try {
           elevation={2} 
           key ={i}>
           <div className='media-box'>
-            <IconButton aria-label='play'>
-              <PlayArrow/>
+            <IconButton aria-label='play' onClick={()=>toggleMedia(i)}>
+             {mediaPlaying ===i?<PauseIcon/>:<PlayArrowIcon/>}
           </IconButton>
           <div>
           <div className='Title'>{item.Title}</div>
