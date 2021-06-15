8376598d6ae614da04de1bef58c7b98fb513cb83
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-06-15 02:30:55 -0500

    added-play-button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React ,{useState,useEffect}from 'react'
 import Amplyfy, { API, graphqlOperation } from 'aws-amplify';
 import awsconfig from "./aws-exports";
 import {AmplifySignOut,withAuthenticator} from '@aws-amplify/ui-react'
-import {listMedias} from './graphql/queries'
+import {listMedias} from './graphql/queries';
+import {Paper,IconButton,} from '@material-ui/core';
+import PlayArrow from '@material-ui/icons/PlayArrow'
 import './App.css';
 Amplyfy.configure(awsconfig)
 function App() {
@@ -29,7 +31,17 @@ try {
        <h2>My app Content</h2>
       </header>
       {media.map((item,i)=>{
-        return(<div key ={i}><h2>{item.Title}</h2></div>)
+        return(<Paper 
+          variant="outlined"
+          elevation={2} 
+          key ={i}>
+          <div className='media-box'>
+            <IconButton aria-label='play'>
+              <PlayArrow/>
+          </IconButton>
+          <div className='Title'>{item.Title}</div>
+          </div>
+          </Paper>)
       })}
     </div>
   );
